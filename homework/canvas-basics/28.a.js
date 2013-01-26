cf452bb21471a590b5415bea7e2ec11eb81ef31e
Britainjs <bsouthwi@lion.lmu.edu>
2013-01-26 02:55:59 -0800

    Added javascript and hmtml for a sunset with a red sun, green ground, grey sky.

---
+++ homework/canvas-basics/28.a.js
@@ -0,0 +1,16 @@
+//A function to draw a simple sunset scene with a red sun setting into a green horizon
+//under a gray-blue sky.
+(function () {
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d");
+    //draw the sky
+    renderingContext.fillStyle = "rgb(176, 196, 222)";
+    renderingContext.fillRect(0, 0, 512, 512);
+    //draw the ground
+    renderingContext.fillStyle = "rgb(0, 200, 0)";
+    renderingContext.fillRect(0, 300, 512, 512);
+    //draw the sun
+    renderingContext.fillStyle = "rgb(247, 78, 5)";
+    renderingContext.arc(256, 300, 60, 0, Math.PI, true);
+    renderingContext.fill();
+}());
