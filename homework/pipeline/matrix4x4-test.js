facdf9f9841ebcca5d4476d39472da9f6f19c3d4
Britainjs <bsouthwi@lion.lmu.edu>
2013-03-24 19:41:52 -0700

    Added unit tests for conversion to column major ordering. Cleaned up the code.

--- homework/pipeline/matrix4x4-test.js
+++ homework/pipeline/matrix4x4-test.js
@@ -16,25 +16,23 @@ $(function () {
         deepEqual(m.elements[4], 0, "Fifth element by index.");
         deepEqual(m.elements[10], 1, "11th element by index.");
         deepEqual(m.elements[15], 1, "Last element by index.");
-        
-        var n = new Matrix4x4(1, 2, 3, 4,
-                               5, 6, 7, 8,
-                               9, 10, 11, 12,
-                               13, 14, 15, 16);
-                               
-        deepEqual(n.elements, [1, 2, 3, 4,
+        m = new Matrix4x4(1, 2, 3, 4,
+                          5, 6, 7, 8,
+                          9, 10, 11, 12,
+                          13, 14, 15, 16);             
+        deepEqual(m.elements, [1, 2, 3, 4,
                                5, 6, 7, 8,
                                9, 10, 11, 12,
                                13, 14, 15, 16], "All elements of an initialized matrix");
-        deepEqual(n.elements[0], 1, "First element by index");
-        deepEqual(n.elements[5], 6, "Sixth element by index");
-        deepEqual(n.elements[11], 12, "Twelfth element by index");
-        deepEqual(n.elements[15], 16, "Last element by index");
+        deepEqual(m.elements[0], 1, "First element by index");
+        deepEqual(m.elements[5], 6, "Sixth element by index");
+        deepEqual(m.elements[11], 12, "Twelfth element by index");
+        deepEqual(m.elements[15], 16, "Last element by index");
 
         m = new Matrix4x4(0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0,
-                           0, 0, 0, 0);
+                          0, 0, 0, 0,
+                          0, 0, 0, 0,
+                          0, 0, 0, 0);
 
         deepEqual(m.elements, [0, 0, 0, 0,
                                0, 0, 0, 0,
@@ -45,7 +43,6 @@ $(function () {
         deepEqual(m.elements[3], 0, "Fourth element by index");
         deepEqual(m.elements[7], 0, "Eighth element by index");
         deepEqual(m.elements[12], 0, "Thirteenth element by index");
-        
     });
 
     test("Multiplication", function () {
@@ -63,76 +60,56 @@ $(function () {
         deepEqual(mresult.elements, [14, 76, 129, 55,
                                      71, 56, 137, 41,
                                      212, 100, 340, 120,
-                                     220, 90, 379, 167]  , "Matrix multiplication");
-        
+                                     220, 90, 379, 167], "Matrix multiplication");
         mresult = m.multiply(o);
         deepEqual(mresult.elements, [0, 4, 5, 1,
                             1, 2, 3, 6,
                             10, 4, 0, 8,
                             15, 2, 1, 1], "Matrix Multiplication by Identity Matrix");
-                            
         mresult = m.multiply(m);
         deepEqual(mresult.elements, [69, 30, 13, 65,
-                                    122, 32, 17, 43,
-                                    124, 64, 70, 42,
-                                    27, 70, 82, 36], 
-                                    "Matrix multiplication using identical matrices.");
+                                     122, 32, 17, 43,
+                                     124, 64, 70, 42,
+                                     27, 70, 82, 36], "Matrix multiplication using identical matrices.");
     });
 
     test("Translate", function () {
         var m1 = getTranslationMatrix(5, 7, 9);
-
         deepEqual(m1.elements, [1, 0, 0, 5,
                                 0, 1, 0, 7,
                                 0, 0, 1, 9,
-                                0, 0, 0, 1], "Translate x by 5, y by 7, and z by 9");
-                                
+                                0, 0, 0, 1], "Translate x by 5, y by 7, and z by 9");      
         m1 = getTranslationMatrix(0, 0, 4);
-        
         deepEqual(m1.elements, [1, 0, 0, 0,
                                 0, 1, 0, 0,
                                 0, 0, 1, 4,
                                 0, 0, 0, 1], "Translate z by 4.");
-        
         m1 = getTranslationMatrix(-5, 0.011, -0.53);
-        
         deepEqual(m1.elements, [1, 0, 0, -5,
                                 0, 1, 0, 0.011,
                                 0, 0, 1, -0.53,
-                                0, 0, 0, 1], 
-                                "Translate x by -5, y by 0.011, and z by -0.53");
+                                0, 0, 0, 1], "Translate x by -5, y by 0.011, and z by -0.53");
         var m2 = new Matrix4x4();
         m1 = getTranslationMatrix(0, 0, 0);
-        
         deepEqual(m1, m2, "Translate by 0, 0, 0 yields the identity matrix.");
-                                            
-                                
-
-
     });
 
     test("Scale", function () {
         var mscale = getScaleMatrix(0.5, 0.5, 0.5);
-        
         deepEqual(mscale.elements, [0.5, 0, 0, 0,
                            0, 0.5, 0, 0,
                            0, 0, 0.5, 0,
                            0, 0, 0, 1], "Scale by 0.5 on all axis.");
-        
         mscale = getScaleMatrix(2, 1, 1);
-        
         deepEqual(mscale.elements, [2, 0, 0, 0,
                            0, 1, 0, 0,
                            0, 0, 1, 0,
                            0, 0, 0, 1], "Scale x value by 2.");
-        
         mscale = getScaleMatrix(5, 3, 2);
-        
         deepEqual(mscale.elements, [5, 0, 0, 0,
                            0, 3, 0, 0,
                            0, 0, 2, 0,
                            0, 0, 0, 1], "Scale by (5, 3, 2).");
-
         // Scaling works by multiplication, so a scale factor  of >= 0 is nonsensical.
         raises(
             function () {
@@ -140,7 +117,6 @@ $(function () {
             },
             "Check for a scale factor of 0."
         );
-        
         raises(
             function () {
                 return getScaleMatrix(2, 3, -1);
@@ -148,31 +124,23 @@ $(function () {
             "Check for a negative scale factor."
         );
     });
-
     test("Rotation", function () {
         var m = getRotationMatrix(45, 1, 0, 0);
-        
         deepEqual(m.elements, [1, 0, 0, 0,
                       0, 0.7071067811865476, -0.7071067811865475, 0,
                       0, 0.7071067811865475, 0.7071067811865476, 0,
                       0, 0, 0, 1], "Rotate about x-axis by 45.");
-        
         m = getRotationMatrix(0, 0, 0, 1);
-        
         deepEqual(m.elements, [1, 0, 0, 0,
                                0, 1, 0, 0,
                                0, 0, 1, 0,
                                0, 0, 0, 1], "Rotate by 0 around the z axis.");
-        
         m = getRotationMatrix(180, 0, 1, 0);
-        
         deepEqual(m.elements, [-1, 0, 1.2246063538223773e-16, 0,
                                 0, 1, 0, 0,
                                 -1.2246063538223773e-16, 0, -1, 0,
                                 0, 0, 0, 1], "Rotate by 180 degrees around the y-axis.");
-        
         m = getRotationMatrix(30, 0, 0, 1);
-        
         deepEqual(m.elements, [0.8660254037844387, -0.49999999999999994, 0, 0,
                                0.49999999999999994, 0.8660254037844387, 0, 0,
                                0, 0, 1, 0,
@@ -182,41 +150,65 @@ $(function () {
     test("Projection", function () {
         //Test Ortho Matrix
         var m = getOrthoMatrix(-1, 1, -1, 1, -1, 1);
-        
         deepEqual(m.elements, [2/(1 - -1), 0, 0, -((1 + -1)/(1 - -1)),
                                0, 2/(1 - (-1)), 0, -((1 + -1)/(1 - -1)),
                                0, 0, (-2)/(1 - -1), -((1 + -1)/(1 - -1)),
                                0, 0, 0, 1], 
                                "Project onto a surface with left -1, right 1, bottom -1, top 1, near -1, and far 1."
         );
-
         m = getOrthoMatrix(-5, 3, 0, 4, -3, 0);
-        
         deepEqual(m.elements, [2/(3 - -5), 0, 0, -((3 + -5)/(3 - -5)),
                                0, 2/(4 - 0), 0, -((4 + 0)/(4 - 0)),
                                0, 0, (-2)/(0 - -3), -((0 + -3)/(0 - -3)),
                                0, 0, 0, 1], 
                                "Project onto a surface of left -5, right 3, bottom 0, top 4, near -3, and far 0."
         );
-        
         //Test Frustum Matrix
         m = getFrustumMatrix(10, 20, 15, 100, 0, 6);
-        
         deepEqual(m.elements, [0, 0, 3, 0,
                                0, 0, 1.3529411764705883, 0,
                                0, 0, -1, 0,
                                0, 0, -1, 0], 
                                "Perspective projection to left 10, right 20, bottom 15, top 100, near 0, far 6."
         );
-        
         m = getFrustumMatrix(-100, 100, -100, 100, -100, 100);
-        
         deepEqual(m.elements, [-1, 0, 0, 0,
                                0, -1, 0, 0,
                                0, 0, 0, 100,
                                0, 0, -1, 0],
                                "Perspective projection to left -100, right 100, bottom -100, top 100, near -100, far 100."
         );      
+    });
+    test("Convenience and Conversion", function () {
+        //Test conversion to column major
+        var m = new Matrix4x4(1, 2, 3, 4,
+                              5, 6, 7, 8,
+                              9, 10, 11, 12,
+                              13, 14, 15, 16);
+        m.toColumnMajor();
+        deepEqual(m.elements, [1, 5, 9, 13,
+                               2, 6, 10, 14,
+                               3, 7, 11, 15,
+                               4, 8, 12, 16], "Test conversion to column major ordering."
+        );
+        m = new Matrix4x4();
+        m.toColumnMajor();
+        deepEqual(m.elements, [1, 0, 0, 0,
+                               0, 1, 0, 0,
+                               0, 0, 1, 0,
+                               0, 0, 0, 1], 
+                               "Identity Matrix converted to column major is unchanged."
+        );
+        m = new Matrix4x4(14, 51, 12, 0,
+                          -1, -3, 5, 23,
+                          145, 5, 6, 0,
+                          435, -1345, 554, 3);
+        m.toColumnMajor();
+        deepEqual(m.elements, [14, -1, 145, 435,
+                               51, -3, 5, -1345,
+                               12, 5, 6, 554,
+                               0, 23, 0, 3],
+                               "Conversion to column major ordering.                              
     });
 
 });
