47bfa87f6162446d26b77414af6dd33a9116170f
britainjs <britainjs@aol.com>
2013-04-16 13:00:50 -0700

    Added a function to make an object orbit.

--- homework/pipeline/animator.js
+++ homework/pipeline/animator.js
@@ -3,8 +3,17 @@ var Animator = {
     hover: function (shapes, dy, rotation) {
             var i;
             for (i = 0; i < shapes.length; i++) {
-                shapes[i].transform.angle = currentRotation;
+                shapes[i].transform.angle = rotation;
                 shapes[i].transform.dy = dy;
             }
+    },
+    
+    //A function to make an object rotate in a circle.
+    orbit: function (shapes, rotation) {
+        var i;
+        for (i = 0; i < shapes.length; i++) {
+            shapes[i].transform.dx = Math.sin (rotation);
+            shapes[i].transform.dz = Math.cos (rotation);
+        }
     }
 };
